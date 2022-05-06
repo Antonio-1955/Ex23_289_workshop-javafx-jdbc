18ab41f47b15b01a6a9d24a9abe4aeaca42aa819
Antonio-1955 <96381553+Antonio-1955@users.noreply.github.com>
2022-05-06 15:45:46 -0300

    Criado o projeto local
    Adicionado MySQLConnector e JDK-1.8 (deveria ser JDC-11)

---
+++ src/ex23_289_workshop/javafx/jdbc/Ex23_289_workshopJavafxJdbc.java
@@ -0,0 +1,34 @@
+/*
+ */
+package ex23_289_workshop.javafx.jdbc;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author anton
+ */
+public class Ex23_289_workshopJavafxJdbc extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
