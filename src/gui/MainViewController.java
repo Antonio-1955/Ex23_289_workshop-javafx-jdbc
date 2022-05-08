e4b92bd8b729706d206d24c1731d7c834525d2af
Antonio-1955 <96381553+Antonio-1955@users.noreply.github.com>
2022-05-08 09:00:52 -0300

    Criação de nova classe
    Adicionada classe DepartmentList.fxml
    Modificada classe MainViewController

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -37,7 +37,8 @@ public class MainViewController implements Initializable {
 
     @FXML
     public void onMenuItemDepartmentAction() {
-        System.out.println("Clicou em 'onMenuItemDepartmentAction'");
+        //System.out.println("Clicou em 'onMenuItemDepartmentAction'");
+        loadView("/gui/DepartmentList.fxml");
     }
 
     @FXML
